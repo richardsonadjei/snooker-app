78d0c98f348fa80aed5e1621c8d3c335937dd2c9
richardsonadjei <adjei.kwadjo@gmail.com>
2024-01-25 20:20:51 +0000

    Create User model

---
+++ api/models/user.model.js
@@ -0,0 +1,50 @@
+// Import required dependencies
+import mongoose from 'mongoose';
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  telephoneNumber: {
+    type: String,
+    required: true
+  },
+  ghanaCardNumber: {
+    type: String,
+    required: true
+  },
+  witnessName: {
+      type: String,
+      required: true
+    },
+  witnessContact: {
+      type: String,
+      required: true
+    },
+  role: {
+    type: String,
+    enum: ['ceo','manager', 'employee',],
+    default: 'employee'
+  },
+},
+{ timestamps: true }
+);
+
+// Create and export the User model
+export default mongoose.model('User', userSchema);
